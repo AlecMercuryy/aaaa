6a28423527bb5eab975aea93c594b492f4750aee
foxyphp <alexssandermor@gmail.com>
2021-07-07 22:02:38 -0300

    first commit

---
+++ src/help.js
@@ -0,0 +1,44 @@
+const help = (prefix) => {
+	return `⌜ *BOT* ⌟  
+
+❍➣ *INFORMACION*
+
+ Comando : ⌜ ${prefix} ⌟
+
+❍➣ *STICKERS*
+  │
+  │⊷️ ${prefix}sticker
+  │⊷️ ${prefix}attp
+  │⊷️  Crea texto de colores
+  │⊷️ ${prefix}emoji 
+  │⊷️  Convierte un emoji a sticker
+  │⊷️ ${prefix}stickergif
+  │⊷️ 6 segundos de video
+  ╰❏ 
+
+❍➣ *CONVERTIDORES*
+  │
+  │⊷️ ${prefix}toimg
+  │⊷️  De sticker a JPG
+  │⊷️ ${prefix}tomp3
+  │⊷️ De video a MP3
+  ╰❏ 
+
+❍➣ *AUDIO*
+  │
+  │⊷️ ${prefix}idioma
+  │⊷️ ${prefix}tts es (No Funcional)
+  ╰❏
+
+❍➣ *OTROS*
+  │
+  │⊷️ ${prefix}wame
+  │⊷️  Link de Whatsapp
+  │⊷️ ${prefix}qrcode
+  │⊷️ Coloca un texto
+  ╰❏ 
+
+`
+}
+
+exports.help = help
